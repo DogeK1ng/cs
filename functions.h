44ecca81832c47bea8d20971b8aba2909d95ad78
DogeK1ng <69004365+DogeK1ng@users.noreply.github.com>
2021-12-14 14:56:59 +0300

    v12

---
+++ functions.h
@@ -0,0 +1,21 @@
+﻿/**
+	*@file
+	*@brief Подключаемый файл, содержит блоки общего кода
+*/
+
+
+void print_hello();
+
+
+b2335effdd3db21f869591085c26c51aa6c86128
+
+
+<<<<<<< HEAD
+=======
+
+>>>>>>> 0511bb616cb665dae5fc10fbe3eb09b9533ba944
+
+void print_hello();
+
+void print_hello();
+void print_hello();
