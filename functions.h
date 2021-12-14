d66365ddf37e3d678a9ecbc36b8530687d5355c1
DogeK1ng <69004365+DogeK1ng@users.noreply.github.com>
2021-12-14 14:41:47 +0300

    v1

--- functions.h
+++ functions.h
@@ -7,9 +7,6 @@
 void print_hello();
 int factorial(int n);
 
-void print_hello();
+b2335effdd3db21f869591085c26c51aa6c86128
 
-<<<<<<< HEAD
-=======
-void print_hello();void print_hello();
->>>>>>> b2335effdd3db21f869591085c26c51aa6c86128
+
