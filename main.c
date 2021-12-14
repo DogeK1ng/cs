d616c78929e50c747987ff40eb71e044b509b43f
DogeK1ng <69004365+DogeK1ng@users.noreply.github.com>
2021-12-14 14:58:47 +0300

    v12

---
+++ main.c
@@ -0,0 +1,17 @@
+
+
+
+#include <stdio.h>
+void world();
+
+/**
+ *@brief Начало приветствия
+ *@detailed Данная функция выводит Hello, , а после обращается к следующей программе
+ *@return 0
+*/
+int main()
+{
+    fprintf(stdout, "Hello, ");
+    world(); 
+    return 0;
+}
